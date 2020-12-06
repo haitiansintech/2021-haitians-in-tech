a2ba5c78c2339ff8d6763f8d9a778ce6561a0b1d
Your Name <annakiyah@DESKTOP-ANNAKIYAH.localdomain>
2020-12-06 17:22:26 -0500

    routing for menu links

--- src/components/header.js
+++ src/components/header.js
@@ -7,12 +7,12 @@ import "../css/navbar.css"
 
 const Header = ({ siteTitle }) => (
   <Navbar className="justify-content-between" bg="primary" variant="dark">
-    <Logo />
+    <div className="nav-logo" href="#home"><Logo />Haitians in Tech</div>
     <Nav className="auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">About</Nav.Link>
-      <Nav.Link href="#pricing">Membership</Nav.Link>      <Nav.Link href="#pricing">Blog</Nav.Link>
-      <Nav.Link href="#pricing">Post a job</Nav.Link>
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/about">About</Nav.Link>
+      <Nav.Link href="/membership">Membership</Nav.Link>      <Nav.Link href="/blog">Blog</Nav.Link>
+      <Nav.Link href="/employer">Post a job</Nav.Link>
     </Nav>
   </Navbar>
 )
