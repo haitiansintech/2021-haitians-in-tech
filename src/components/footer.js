5f3d180238eadd8e8be2b245dea60277e3a519af
Whitney <whitneylubin@icloud.com>
2021-01-02 22:31:22 -0500

    added media query for footer font-awesome icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -39,22 +39,22 @@ const Footer = () => (
                     <div className="social-media">
                         <div>
                             <a rel="noreferrer" target="_blank" href="https://github.com/haitiansintech">
-                                <FontAwesomeIcon className="social-icons" icon={faGithubSquare} />
+                                <FontAwesomeIcon className="social-icons glyphicon" icon={faGithubSquare} />
                             </a>
                         </div>
                         <div>
                             <a rel="noreferrer" target="_blank" href="https://twitter.com/haitiansintech">
-                                <FontAwesomeIcon className="social-icons" icon={faTwitterSquare} />
+                                <FontAwesomeIcon className="social-icons glyphicon" icon={faTwitterSquare} />
                             </a>
                         </div>
                         <div>
                             <a rel="noreferrer" target="_blank" href="https://instagram.com/haitiansintech">
-                                <FontAwesomeIcon className="social-icons" icon={faInstagramSquare} />
+                                <FontAwesomeIcon className="social-icons glyphicon" icon={faInstagramSquare} />
                             </a>
                         </div>
                         <div>
                             <a rel="noreferrer" target="_blank" href="https://twitch.com/haitiansintech">
-                                <FontAwesomeIcon className="social-icons" icon={faTwitch} />
+                                <FontAwesomeIcon className="social-icons glyphicon" icon={faTwitch} />
                             </a>
                         </div>
                     </div>
